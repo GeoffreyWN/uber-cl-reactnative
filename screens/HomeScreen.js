9565643d277fb3911132c5230440841954fba5a5
weshsnr <ndungugeoffrey11@gmail.com>
2021-08-01 18:28:59 +0300

    added googleplacesautocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GOOGLE_MAPS_APIKEY } from '@env' //configured in babel.config.js
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { useDispatch } from 'react-redux';
+import { setOrigin, setDestination } from '../slices/navSlice'
+
 
 const HomeScreen = () => {
-    
+    const dispatch = useDispatch()
     return (
         <SafeAreaView style={tw`bg-white h-full`} >
             <View style={tw`p-5`} >
@@ -15,7 +20,25 @@ const HomeScreen = () => {
                     source={{
                         uri: 'https://links.papareact.com/gzs'
                     }} />
-                    <NavOptions/>
+
+                <GooglePlacesAutocomplete
+                    styles={{ container: { flex: 0 }, textInput: { fontSize: 18 } }}
+                    enablePoweredByContainer={false}
+                    onPress={(data, details = null) => {
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+                        dispatch(setDestination(null))
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={"search"}
+                    query={{ key: GOOGLE_MAPS_APIKEY, language: 'en' }}
+                    placeholder="Where from?"
+                    minLength={2}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400} />
+                <NavOptions />
             </View>
         </SafeAreaView>
     )
